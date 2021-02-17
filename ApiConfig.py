055688f21d581e80e9f30aa14b7734f39be88119
“AligoXOC“ <sebastian.larrea@aligo.com.co>
2021-02-17 16:06:46 -0500

    “archivo”

---
+++ ApiConfig.py
@@ -0,0 +1,11 @@
+#TheHive4Py Configuration File
+
+ApiConfig = {
+    "TheHiveHost": "http://192.168.100.105",
+    "TheHivePort": "9002",
+    "TheHiveApiKey": "FejzI7LCwqljhIiNjEEqbggVdB0yeO0A",
+    "CortexHost": "http://192.168.100.99",
+    "CortexPort": "9001",
+    "CortexApiKey": "FXILcJUVKeX5fyniu9ViHTK0tzeVQK4y",
+    "CortexId": "CORTEX-SERVER"
+}
