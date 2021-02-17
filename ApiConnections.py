83a4b3f26797b5296158fcb7c08c5c4811ce9376
“AligoXOC“ <sebastian.larrea@aligo.com.co>
2021-02-17 16:07:42 -0500

    archivo

---
+++ ApiConnections.py
@@ -0,0 +1,20 @@
+import time
+
+# Config File
+import ApiConfig as cfg
+
+# TheHive4Py Imports
+from thehive4py.api import TheHiveApi
+from thehive4py.models import *
+
+#Cortex Imports
+from cortex4py.api import Api
+
+#APIs Connection
+hiveServer = cfg.ApiConfig["TheHiveHost"] + ":" + cfg.ApiConfig["TheHivePort"]
+hiveKey = cfg.ApiConfig["TheHiveApiKey"]
+apiH = TheHiveApi(hiveServer, hiveKey)
+
+cortexServer = cfg.ApiConfig["CortexHost"] + ":" + cfg.ApiConfig["CortexPort"]
+cortexKey = cfg.ApiConfig["CortexApiKey"]
+apiC = Api(cortexServer, cortexKey)
